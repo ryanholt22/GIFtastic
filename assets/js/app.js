e7398710e86cc448079d9cb7d8c609c1d3a7d4e1
Ryan Holt <ryanholt22@yahoo.com>
2019-08-06 18:33:13 -0500

    fixed some js but not all of it

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
-var topics = ["GSXR1000", "CBR1000", "CBRF4i", "ZX6R", "H2R", "R1", "R6" ]
+renderButtons();
 
-function renderButtons () {
+var topics = ["GSXR1000", "CBR1000", "CBRF4i", "ZX6R", "H2R", "R1", "R6"]
+
+function renderButtons() {
   $("#bikes").empty();
   for (i = 0; i < topics.length; i++) {
-    $(".bikes").append("<button>" + topics[i] + "</button>");
+    var w = $("<a>");
+    w.attr("data-bike", topics[i]);
+    w.text(topics[i]);
+    $(".button").append(w);
   }
-}
+};
 
 $("#button").on("click", function () {
   event.preventDefault();
@@ -15,46 +20,46 @@ $("#button").on("click", function () {
   return;
 });
 
-$("button").on("click", function () {
+$(".button").on("click", function () {
   var bike = $(this).attr("data-bike");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    bike + "&api_key=Tg1gRmZCHTSoNpDpVHBxbxP4eWMXtNe4&limit=10"
-    })
+    bike + "&api_key=Tg1gRmZCHTSoNpDpVHBxbxP4eWMXtNe4&limit=10";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }) .then(function(response) {
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
       var results = response.data;
-     $("#bikes").empty();
-     for (i =0; i < results.length; i++) {
-      var bikeDiv = $("<div>");
-      var p = $("<p>").text("Rating: " + results[i].rating);
-      var bikeImg = $("<img>");
-      bikeImg.attr("src", results[i].images.original_still.url);
-      bikeImg.attr("data-still", results[i].images.original_still.url);
-      bikeImg.attr("data-animate", results[i].images.original.url);
-      bikeImg.attr("data-state", "still");
-      bikeImg.attr("class", "gif");
-      bikeDiv.append(p);
-      bikeDiv.append(bikeImg);
-      $("#bikes").append(bikeDiv);
-    }
-  });
-
-  function changeState(){
-    var state = $(this).attr("data-state");
-    var animateImg = $(this).attr("data-still");
-
-    if (state == "still") {
-      $(this).attr("src", animateImg);
-      $(this).attr("data-state", "animate");
+      $("#bikes").empty();
+      for (i = 0; i < results.length; i++) {
+        var bikeDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        var bikeImg = $("<img>");
+        bikeImg.attr("src", results[i].images.original_still.url);
+        bikeImg.attr("data-still", results[i].images.original_still.url);
+        bikeImg.attr("data-animate", results[i].images.original.url);
+        bikeImg.attr("data-state", "still");
+        bikeImg.attr("class", "gif");
+        bikeDiv.append(p);
+        bikeDiv.append(bikeImg);
+        $("#bikes").append(bikeDiv);
       }
-      else if (state == "animate") {
-        $(this).attr("src", stillImage);
-        $(this).attr("data-state", "still");
-      }
-    }
-    $(document).on("click", "gif", changeState);
-  
+    });
+});
+
+function changeState() {
+  var state = $(this).attr("data-state");
+  var animateImg = $(this).attr("data-still");
+
+  if (state == "still") {
+    $(this).attr("src", animateImg);
+    $(this).attr("data-state", "animate");
+  }
+  else if (state == "animate") {
+    $(this).attr("src", stillImage);
+    $(this).attr("data-state", "still");
+  }
+}
+$(document).on("click", "gif", changeState);
+
 
